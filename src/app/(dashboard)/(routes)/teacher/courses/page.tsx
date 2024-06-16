d0a9d00977249c82d08592f8256098005056ca49
dhruvinvaghani001 <dhruvinvaghani0011@gmail.com>
2024-06-16 19:45:15 +0530

    course data tables added

--- src/app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -3,22 +3,57 @@ import { db } from '@/lib/db'
 import { auth } from '@clerk/nextjs/server'
 import Link from 'next/link'
 import React from 'react'
+import { DataTable } from './_components/CourseTable'
+import { columns } from './_components/Columns'
+import { redirect } from 'next/navigation'
+import { PlusCircle } from 'lucide-react'
 
-type Props = {}
 
-const page = async (props: Props) => {
+
+async function getData(): Promise<[]> {
+    // Fetch data from your API here.
+    return [
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "m@example.com",
+        },
+        // ...
+    ]
+}
+
+
+const page = async () => {
 
     const { userId } = auth();
 
+    if (!userId) {
+        redirect("/");
+    }
 
+    const courses = await db.course.findMany({
+        where: {
+            userId: userId,
+        },
+        orderBy: {
+            createdAt: "desc",
+        }
+    })
+
+    const data = await getData()
 
 
     return (
         <div className=''>
-            <div className="main__section flex justify-start items-center p-8">
-                <Link href="/teacher/create">
-                    <Button variant="default" className="bg-secondary-foreground">Craete New</Button>
+            <div className="main__section p-10 flex flex-col  gap-4">
+                <Link href="/teacher/create" className='w-fit'>
+                    <Button variant="default" className="bg-secondary-foreground flex items-center
+                    gap-2"><PlusCircle /> Create New</Button>
                 </Link>
+                <div>
+                    <DataTable columns={columns} data={courses} />
+                </div>
             </div>
         </div>
     )
