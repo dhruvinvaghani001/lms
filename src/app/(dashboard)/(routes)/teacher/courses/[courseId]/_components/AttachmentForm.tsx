fc1f74784c31eb46e71151f302b83e1378cf6080
dhruvinvaghani001 <dhruvinvaghani0011@gmail.com>
2024-06-17 18:44:43 +0530

    type error fix

--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/AttachmentForm.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/AttachmentForm.tsx
@@ -1,141 +1,145 @@
 "use client";
-import React, { useState } from 'react';
-import { zodResolver } from "@hookform/resolvers/zod"
+import React, { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import z from "zod";
 
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
-import { Button } from '@/components/ui/button';
-import { ImageIcon, PenLine, PlusCircle, File, X, XCircle, Loader2 } from 'lucide-react';
-import { Card } from '@/components/ui/card';
+import { Button } from "@/components/ui/button";
+import { PlusCircle, File, X, XCircle, Loader2 } from "lucide-react";
+import { Card } from "@/components/ui/card";
 
+import toast from "react-hot-toast";
 
-import toast from 'react-hot-toast';
-
-import axios from 'axios';
-import { useRouter } from 'next/navigation';
-import { Textarea } from '@/components/ui/textarea';
-import FielUpload from '@/components/FielUpload';
-import Image from 'next/image';
-import { Attachment } from '@prisma/client';
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { Textarea } from "@/components/ui/textarea";
+import FielUpload from "@/components/FielUpload";
+import Image from "next/image";
+import { Attachment } from "@prisma/client";
 
 interface AttchmentFormProps {
-    attachments: Attachment[];
-    courseId: string,
+  attachments: Attachment[];
+  courseId: string;
 }
-
 
 const formSchema = z.object({
-    url: z.string().min(1),
-})
+  url: z.string().min(1),
+});
 
 const AttchmentForm = ({ attachments, courseId }: AttchmentFormProps) => {
-    const router = useRouter();
-    const [isEditing, setIsEditting] = useState(false);
-    const [deleteId, setdeleteId] = useState<string | null>(null);
+  const router = useRouter();
+  const [isEditing, setIsEditting] = useState(false);
+  const [deleteId, setdeleteId] = useState<string | null>(null);
 
-    const toggleEdit = () => {
-        setIsEditting(prev => !prev);
+  const toggleEdit = () => {
+    setIsEditting((prev) => !prev);
+  };
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      url: "",
+    },
+  });
+
+  const { isValid, isSubmitting } = form.formState;
+
+  // to add attachments to course
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const response = await axios.post(
+        `/api/courses/${courseId}/attachments`,
+        values
+      );
+      toast.success(response.data.message);
+      console.log(response);
+      toggleEdit();
+      router.refresh();
+    } catch (error:any) {
+      console.log("title update error", error);
+      toast.error(error?.message);
     }
+  };
 
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            url: ""
-        }
-    })
-
-    const { isValid, isSubmitting } = form.formState;
-
-
-    // to add attachments to course 
-    const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        try {
-            const response = await axios.post(`/api/courses/${courseId}/attachments`, values);
-            toast.success(response.data.message);
-            console.log(response);
-            toggleEdit();
-            router.refresh();
-        } catch (error: any) {
-            console.log("title update error", error);
-            toast.error(error?.message);
-        }
+  // delete attachments
+  const onDelete = async (id: string) => {
+    try {
+      const response = await axios.delete(
+        `/api/courses/${courseId}/attachments/${id}`
+      );
+      toast.success(response.data.message);
+      console.log(response);
+      router.refresh();
+    } catch (error:any) {
+      console.log(error);
+      toast.error(error?.message);
     }
+  };
 
-
-    // delete attachments 
-    const onDelete = async (id: string) => {
-        try {
-            const response = await axios.delete(`/api/courses/${courseId}/attachments/${id}`);
-            toast.success(response.data.message);
-            console.log(response);
-            router.refresh();
-        } catch (error: any) {
-            console.log(error);
-            toast.error(error?.message);
-        }
-    }
-
-    return (
-        <div>
-            <Card className='w-full'>
-                <div className='bg-card rounded-md p-4'>
-                    <div className='flex justify-between gap-2 items-center'>
-                        Course Attachments
-                        <Button variant="ghost" className='flex gap-2' onClick={toggleEdit}>
-                            {
-                                isEditing && <>Cancel</>
-                            }
-                            {
-                                !isEditing && (
-                                    <>
-                                        <PlusCircle />
-                                        Add a file
-                                    </>
-                                )
-                            }
-                        </Button>
-                    </div>
-                    <div className="form mt-2">
-                        {
-                            !isEditing && (attachments.length === 0 && <p className='text-sm mt-2'>No attachments yet</p>)
-                        }
-                        {
-                            !isEditing && (attachments.length > 0 && <div className='mt-4'>
-                                <div className="flex flex-col gap-3">
-                                    {
-                                        attachments.map((attach, index) => {
-                                            return <div className='p-2 bg-muted rounded-lg flex justify-between' key={index}>
-                                                <div className="flex items-center gap-4"> <File />
-                                                    {attach.name}</div>
-                                                {deleteId === attach.id && <Loader2 />}
-                                                {
-                                                    deleteId !== attach.id && <XCircle onClick={() => {
-                                                        setdeleteId(attach.id);
-                                                        onDelete(attach.id)
-                                                    }} />
-                                                }
-                                            </div>
-                                        })
-                                    }
-                                </div>
-                            </div>)
-                        }
-                        {
-                            isEditing && <FielUpload
-                                endpoint='courseAttachment'
-                                onChange={(url) => {
-                                    if (url) {
-                                        onSubmit({ url: url })
-                                    }
-                                }}
-                            />
-                        }
-                    </div>
+  return (
+    <div>
+      <Card className="w-full">
+        <div className="bg-card rounded-md p-4">
+          <div className="flex justify-between gap-2 items-center">
+            Course Attachments
+            <Button variant="ghost" className="flex gap-2" onClick={toggleEdit}>
+              {isEditing && <>Cancel</>}
+              {!isEditing && (
+                <>
+                  <PlusCircle />
+                  Add a file
+                </>
+              )}
+            </Button>
+          </div>
+          <div className="form mt-2">
+            {!isEditing && attachments.length === 0 && (
+              <p className="text-sm mt-2">No attachments yet</p>
+            )}
+            {!isEditing && attachments.length > 0 && (
+              <div className="mt-4">
+                <div className="flex flex-col gap-3">
+                  {attachments.map((attach, index) => {
+                    return (
+                      <div
+                        className="p-2 bg-muted rounded-lg flex justify-between"
+                        key={index}
+                      >
+                        <div className="flex items-center gap-4">
+                          {" "}
+                          <File />
+                          {attach.name}
+                        </div>
+                        {deleteId === attach.id && <Loader2 />}
+                        {deleteId !== attach.id && (
+                          <XCircle
+                            onClick={() => {
+                              setdeleteId(attach.id);
+                              onDelete(attach.id);
+                            }}
+                          />
+                        )}
+                      </div>
+                    );
+                  })}
                 </div>
-            </Card >
-        </div >
-    )
-}
+              </div>
+            )}
+            {isEditing && (
+              <FielUpload
+                endpoint="courseAttachment"
+                onChange={(url) => {
+                  if (url) {
+                    onSubmit({ url: url });
+                  }
+                }}
+              />
+            )}
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+};
 
-export default AttchmentForm
+export default AttchmentForm;
--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/CategoryForm.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/CategoryForm.tsx
@@ -1,117 +1,132 @@
 "use client";
-import React, { useState } from 'react';
-import { zodResolver } from "@hookform/resolvers/zod"
+import React, { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import z from "zod";
 
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
-import { Button } from '@/components/ui/button';
-import { PenLine } from 'lucide-react';
-import { Card } from '@/components/ui/card';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { PenLine } from "lucide-react";
+import { Card } from "@/components/ui/card";
 
+import toast from "react-hot-toast";
 
-import toast from 'react-hot-toast';
-
-import axios from 'axios';
-import { useRouter } from 'next/navigation';
-import { Textarea } from '@/components/ui/textarea';
-import { Combobox } from '@/components/ui/combobox';
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { Textarea } from "@/components/ui/textarea";
+import { Combobox } from "@/components/ui/combobox";
 
 interface CategoryFormProps {
-    categoryId: string | null,
-    courseId: string,
-    options: { label: string, value: string }[]
+  categoryId: string | null;
+  courseId: string;
+  options: { label: string; value: string }[];
 }
-
 
 const formSchema = z.object({
-    categoryId: z.string().min(1),
-})
+  categoryId: z.string().min(1),
+});
 
 const CategoryForm = ({ categoryId, courseId, options }: CategoryFormProps) => {
+  const router = useRouter();
+  const [isEditing, setIsEditting] = useState(false);
 
-    const router = useRouter();
-    const [isEditing, setIsEditting] = useState(false);
+  const toggleEdit = () => {
+    setIsEditting((prev) => !prev);
+  };
 
-    const toggleEdit = () => {
-        setIsEditting(prev => !prev);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      categoryId: categoryId || "",
+    },
+  });
+
+  const { isValid, isSubmitting } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      console.log(values);
+      const response = await axios.patch(`/api/courses/${courseId}`, values);
+      toast.success(response.data.message);
+      console.log(response);
+      toggleEdit();
+      router.refresh();
+    } catch (error:any) {
+      console.log("title update error", error);
+      toast.error(error?.message);
     }
+  };
 
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            categoryId: categoryId || ""
-        }
-    })
+  const selectedCategory = options.find(
+    (option) => option.value === categoryId
+  );
+  console.log(selectedCategory);
 
-    const { isValid, isSubmitting } = form.formState;
-
-    const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        try {
-            console.log(values);
-            const response = await axios.patch(`/api/courses/${courseId}`, values);
-            toast.success(response.data.message);
-            console.log(response);
-            toggleEdit();
-            router.refresh();
-        } catch (error: any) {
-            console.log("title update error", error);
-            toast.error(error?.message);
-        }
-    }
-
-    const selectedCategory = options.find((option) => option.value === categoryId);
-    console.log(selectedCategory);
-
-    return (
-        <div>
-            <Card className='w-full'>
-                <div className='bg-card rounded-md p-4'>
-                    <div className='flex justify-between gap-2 items-center'>
-                        Course Category
-                        <Button variant="ghost" className='flex gap-2' onClick={toggleEdit}>
-                            {
-                                isEditing && <>Cancel</>
-                            }
-                            {
-                                !isEditing && <> <PenLine />
-                                    Edit Category</>
-                            }
-                        </Button>
-                    </div>
-                    <div className="form mt-2">
-                        {
-                            !isEditing && <p className='italic font-semibold'>{selectedCategory?.label || "No category"} </p>
-                        }
-                        {
-                            isEditing && <Form {...form}>
-                                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                                    <FormField
-                                        control={form.control}
-                                        name="categoryId"
-                                        render={({ field }) => (
-                                            <FormItem>
-                                                <FormControl>
-                                                    <Combobox options={options} {...field} />
-                                                </FormControl>
-                                                <FormDescription>
-                                                    What will you teach in this course ?
-                                                </FormDescription>
-                                                <FormMessage />
-                                            </FormItem>
-                                        )}
-                                    />
-                                    <div className=''>
-                                        <Button disabled={isSubmitting || !isValid} type="submit">Save</Button>
-                                    </div>
-                                </form>
-                            </Form>
-                        }
-                    </div>
-                </div>
-            </Card >
-        </div >
-    )
-}
+  return (
+    <div>
+      <Card className="w-full">
+        <div className="bg-card rounded-md p-4">
+          <div className="flex justify-between gap-2 items-center">
+            Course Category
+            <Button variant="ghost" className="flex gap-2" onClick={toggleEdit}>
+              {isEditing && <>Cancel</>}
+              {!isEditing && (
+                <>
+                  {" "}
+                  <PenLine />
+                  Edit Category
+                </>
+              )}
+            </Button>
+          </div>
+          <div className="form mt-2">
+            {!isEditing && (
+              <p className="italic font-semibold">
+                {selectedCategory?.label || "No category"}{" "}
+              </p>
+            )}
+            {isEditing && (
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="space-y-8"
+                >
+                  <FormField
+                    control={form.control}
+                    name="categoryId"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <Combobox options={options} {...field} />
+                        </FormControl>
+                        <FormDescription>
+                          What will you teach in this course ?
+                        </FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <div className="">
+                    <Button disabled={isSubmitting || !isValid} type="submit">
+                      Save
+                    </Button>
+                  </div>
+                </form>
+              </Form>
+            )}
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+};
 
 export default CategoryForm;
