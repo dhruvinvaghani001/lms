71d0d6905a505a8ead5dfc2b2964e537fccbf967
dhruvinvaghani001 <dhruvinvaghani0011@gmail.com>
2024-07-01 15:05:10 +0530

    add image in metadata

--- src/app/(auth)/(routes)/layout.tsx
+++ src/app/(auth)/(routes)/layout.tsx
@@ -4,6 +4,11 @@ import React from "react";
 export const metadata: Metadata = {
   title: "Continue with LMS",
   description: "authenticate your self to continue yout journey with lms",
+  openGraph: {
+    images: [
+      "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/8e3859178674563.6672a0a942de1.jpg",
+    ],
+  },
 };
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
